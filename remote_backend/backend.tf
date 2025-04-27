b8f165aade113bf0bbcabb1f4dc9ef2065a73b57
MiriamOra <miriamoranu@gmail.com>
2025-04-27 00:05:25 -0700

    remove bucket and lock name

--- remote_backend/backend.tf
+++ remote_backend/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "w6-contract-673008-terraformstatefile-2"
+    bucket = "w6-con**GET-BUCKET***-2"
     region = "us-east-1"
     key    = "dev/terraform.tfstate"
-    dynamodb_table = "lock-w6-contract-673008-terraformstatefile-2"
+    dynamodb_table = "lock-w6-**GET-TABLE***-2"
     encrypt = true
   }
 }
