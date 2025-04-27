362ed581907ff63733b743989a4b27993e313ded
MiriamOra <miriamoranu@gmail.com>
2025-04-27 00:11:26 -0700

    update

--- remote_backend/backend.tf
+++ remote_backend/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "w6-con**GET-BUCKET***-2"
+    bucket = "w6-ADD_BUCKET-2"
     region = "us-east-1"
     key    = "dev/terraform.tfstate"
-    dynamodb_table = "lock-w6-**GET-TABLE***-2"
+    dynamodb_table = "lock-ADD_TABLE-2"
     encrypt = true
   }
 }
